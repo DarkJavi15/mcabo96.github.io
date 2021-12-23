173f388d8e959f97031793f32f1b7c01dec8084d
Javi <jcazorla500@gmail.com>
2021-12-23 22:28:51 +0100

    Primera version estable movil

--- main.js
+++ main.js
@@ -10,5 +10,21 @@ function myFunction(x) {
         document.getElementById("mySidenav").style.width = "100%";
     } else {
         document.getElementById("mySidenav").style.width = "0";
+        
+        
+    }
+}
+
+
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+document.getElementById("demo").innerHTML="";
+window.onload =  setTimeout(typeWriter, 700);
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
     }
 }
