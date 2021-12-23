2f65e0cc3e2393e10921fdcadcb03cb100605036
Javi <jcazorla500@gmail.com>
2021-12-23 19:38:20 +0100

    Primera version estable movil

--- main.js
+++ main.js
@@ -5,4 +5,10 @@
 /*Icono hamburguesa*/
 function myFunction(x) {
     x.classList.toggle("change");
+    let longitud = '0px';
+    if (document.getElementById("mySidenav").style.width==0||document.getElementById("mySidenav").style.width==longitud) {
+        document.getElementById("mySidenav").style.width = "100%";
+    } else {
+        document.getElementById("mySidenav").style.width = "0";
+    }
 }
